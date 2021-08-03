704a6555065907f763e301431233f733d0caa6a5
black <jarukit.suchat@gmail.com>
2021-08-03 23:25:38 +0700

    update

--- main.js
+++ main.js
@@ -1,19 +1,81 @@
-// import express from "express";
-// import ptp from "pdf-to-printer";
-// import fs from "fs";
-// import path from "path";
 
 const express = require('express');
 const ptp = require('pdf-to-printer');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
+const http = require('http');
+const https = require('https'); // or 'https' for https:// URLs
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const QRCode = require('qrcode')
+
 const port = 3000;
+const qrcodeTimeout = 60000;
+const printInterval = 1000
+const kiosk_name = "KEDOKI2"
 
-ptp
-  .getPrinters()
-  .then(console.log)
-  .catch(console.error);
+// app.use(express.static('public'))
 
-ptp.print('202100000002-13.pdf', {printer: "EPSON_L3150_Series_2"});
+function genQr(){
+  QRCode.toDataURL(Buffer.from(JSON.stringify({"kiosk_name" : kiosk_name, "create_date" : new Date().getTime()})).toString('base64'), function (err, url) {
+    // console.log(url)
+    io.emit('qrcode', url);
+  })
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  // console.log('a user connected');
+  io.emit('kiosk_name', kiosk_name);
+  io.emit('status', "Ready");
+  QRCode.toDataURL(Buffer.from(JSON.stringify({"kiosk_name" : kiosk_name, "create_date" : new Date().getTime()})).toString('base64'), function (err, url) {
+    io.emit('qrcode', url);
+  })
+});
+
+server.listen(port, () => {
+  console.log('listening on *:',port);
+});
+
+var qrInterval = setInterval(genQr,qrcodeTimeout)
+
+setInterval(function(){
+    
+    // console.log("check")
+    var request = https.get("https://edoc.opencloudai.com/api/kiosk/print.php?kiosk_name="+kiosk_name, function(response) {
+      response.on('data', function (chunk) {
+        // console.log(chunk.toString())
+        data = JSON.parse(chunk)
+        if(data){
+          io.emit('status', "Printing");
+          
+          setTimeout(function(){
+            clearInterval(qrInterval)
+            genQr()
+            io.emit('status', "Ready");
+            qrInterval = setInterval(genQr,qrcodeTimeout)
+          },5000)
+          // console.log(data["document_url"]);
+          // var request = https.get(data["document_url"], function(response) {
+          // var file = fs.createWriteStream("tempfile");
+          //   response.pipe(file);
+          //   ptp
+          //   .getPrinters()
+          //   .then(console.log)
+          //   .catch(console.error);
+
+          //   ptp.print('tempfile', {printer: "EPSON_L3150_Series_2"});
+          // });
+        }
+      });
+    })
+},printInterval)
+
+
+
+
